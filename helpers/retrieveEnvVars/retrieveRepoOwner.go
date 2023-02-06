fdf66d7cbdc0b26a91d976d1c6fcd2945316cd46
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-02-06 14:57:56 -0300

    Update on env vars

--- helpers/retrieveEnvVars/retrieveRepoOwner.go
+++ helpers/retrieveEnvVars/retrieveRepoOwner.go
@@ -3,13 +3,16 @@ package retrieveEnvVars
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func RetrieveRepoOwner() (string, error) {
-	owner := os.Getenv("PULL_REQUEST_OWNER")
-	if owner == "" {
-		return "", fmt.Errorf("PULL_REQUEST_OWNER environment variable not set")
+	info := os.Getenv("PULL_REQUEST_INFO")
+	if info == "" {
+		return "", fmt.Errorf("PULL_REQUEST_INFO environment variable not set")
 	}
+
+	owner := strings.Split(info, "/")[0]
 
 	return owner, nil
 }
