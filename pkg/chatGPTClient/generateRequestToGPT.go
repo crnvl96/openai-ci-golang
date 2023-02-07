82e086669ed39e20edf7d74fbeb488d47ea154d2
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-02-06 22:49:30 -0300

    test

--- pkg/chatGPTClient/generateRequestToGPT.go
+++ pkg/chatGPTClient/generateRequestToGPT.go
@@ -1,17 +1,18 @@
 package chatGPTClient
 
 import (
-	"fmt"
-
 	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 func GenerateRequestToGPT(content string) gogpt.CompletionRequest {
+	t := "func Sum(a, b int) int {\n\treturn a + b\n}"
+
 	request := gogpt.CompletionRequest{
-		Model:       gogpt.GPT3Davinci,
-		MaxTokens:   1028,
+		Model:       gogpt.GPT3TextDavinci002,
+		MaxTokens:   2048,
 		Temperature: 0.5,
-		Prompt:      fmt.Sprintf("Explain Code:\n %v", content),
+		Prompt:      t,
+		// Prompt:      fmt.Sprintf("Explain Code:\n %s", content),
 	}
 
 	return request
